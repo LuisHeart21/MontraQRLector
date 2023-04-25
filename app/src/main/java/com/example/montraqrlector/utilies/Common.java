d61c882544103d1b672b44334505f8b8152e46d0
Luis Angel Ramirez Cruz <75470410+LuisHeart21@users.noreply.github.com>
2023-04-25 13:16:59 -0600

    Subida 4

---
+++ app/src/main/java/com/example/montraqrlector/utilies/Common.java
@@ -0,0 +1,15 @@
+package com.example.montraqrlector.utilies;
+
+import com.example.montraqrlector.models.GoogleSheetsResponse;
+import com.example.montraqrlector.models.IGoogleSheets;
+
+public class Common {
+    public static String BASE_URL = "https://script.google.com/macros/s/AKfycbxGedH2kA6oKVXZfqnxpI1GYUN64bhOtcUXHlPgCOrRzrkLe1hmemYw4CLyxcA055gfvg/";
+    public static String GOOGLE_SHEET_ID = "1iMiaZyx7zKYY-2wncLKm-WSQllWYKBxG_qpx3R3j9pE";
+    public static String SHEET_NAME = "personas";
+
+    public static IGoogleSheets iGSGetMethodClient(String baseUrl) {
+        return GoogleSheetsResponse.getClientGetMethod(baseUrl)
+                .create(IGoogleSheets.class);
+    }
+}
